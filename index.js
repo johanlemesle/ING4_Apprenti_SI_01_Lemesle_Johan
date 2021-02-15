1903c55c03ccb04153a4ad4f989f05c82b1576cd
Johan <johan.lemesle@outlook.fr>
2021-02-16 00:51:51 +0100

    up

--- index.js
+++ index.js
@@ -1,46 +1,70 @@
 const express = require('express');
 const app = express();
-
-app.get('/', (req, res) => {
-    const response = {
-        message: 'Hello World!',
-    };
-
-    res.send(JSON.stringify(response));
-});
-
-/*app.get('/users', function(req, res) {
-  const users = [
-    {
-       id: '1234',
-       name: 'user1',
-    },
-    {
-      id: '1234',
-      name: 'user1',
-    },
-  ];
-  
-  return res.status(200).json(users);
-});*/
-
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.get('/users/:id', function (req, res) {
-    const userId = req.params.id;
+app.get('/', (req, res) => {
+  const response = {
+    message: 'Hello World!',
+  };
 
+  res.send(JSON.stringify(response));
+});
+
+
+app.get('/users', function (req, res) {
+  const users = [
+    {
+      id: 1,
+      name: 'user1',
+    },
+    {
+      id: 2,
+      name: 'user2',
+    },
+    {
+      id: 3,
+      name: 'user3',
+    },
+  ];
+
+  return res.status(200).json(users);
+});
+
+function waza(elem) {
+  return elem.id === 1;
+}
+
+app.get('/users/:id(\\d+)', function (req, res) {
+  const userId = req.params.id;
+
+  if (users.find(e => e.id === 1))
+  {
     return res.status(200).json(userId);
+  }
+  else {
+    next(new Error('404'));
+  }
+
 });
 
 
 app.post('/users', (req, res) => {
-    const body = req.body;
+  const body = req.body;
 
-    return res.status(201).json(body);
+  return res.status(201).json(body);
+});
+
+
+app.put('/', function (req, res) {
+  // PUT
+});
+
+app.delete('/', (req, res) => {
+  // DELETE
 });
 
 app.listen(8000, function () {
-    console.log(`server listening on 8000`);
+  console.log(`server listening on 8000`);
 });

ce3b622492dd52dd0488d73391a03230ba0c70eb
Johan <johan.lemesle@outlook.fr>
2021-04-15 21:16:01 +0200

    up

--- app.js
+++ app.js
@@ -67,11 +67,42 @@ app.post('/login', (req, res) => {
     return res.json({ access_token: token })
 })
 
+/* Récupération du header bearer */
+const extractBearerToken = headerValue => {
+    if (typeof headerValue !== 'string') {
+        return false
+    }
+
+    const matches = headerValue.match(/(bearer)\s+(\S+)/i)
+    return matches && matches[2]
+}
+
+/* Vérification du token */
+const checkTokenMiddleware = (req, res, next) => {
+    // Récupération du token
+    const token = req.headers.authorization && extractBearerToken(req.headers.authorization)
+
+    // Présence d'un token
+    if (!token) {
+        return res.status(401).json({ message: 'Error. Need a token' })
+    }
+
+    // Véracité du token
+    jwt.verify(token, SECRET, (err, decodedToken) => {
+        if (err) {
+            res.status(401).json({ message: 'Error. Bad token' })
+        } else {
+            return next()
+        }
+    })
+}
+
 
 const channelRoutes = require('./app/routes/channel_routes');
 const userRoutes = require('./app/routes/user_routes');
 const messageRoutes = require('./app/routes/message_routes');
 
+app.use(checkTokenMiddleware);
 app.use('/api/v1/channels', channelRoutes);
 app.use('/api/v1/users', userRoutes);
 app.use('/api/v1', messageRoutes);

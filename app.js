ee09f69c308ea4fbc26b35cf74274f8b7c663c38
Johan <johan.lemesle@outlook.fr>
2021-04-15 20:09:18 +0200

    up

---
+++ app.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const app = express();
+
+
+
+
+// Chargement des modules
+const morgan = require('morgan')
+const jwt = require('jsonwebtoken')
+const cors = require('cors')
+
+
+const SECRET = 'mykey'
+
+app.use(cors())                                 // Activation de CORS
+app.use(morgan('tiny'))                         // Activation de Morgan
+app.use(express.json())                         // Activation du raw (json)
+app.use(express.urlencoded({ extended: true })) // Activation de x-wwww-form-urlencoded
+
+// Liste des utilisateurs
+const users = [
+    { id: 1, username: 'admin', password: 'password123' }
+]
+
+/* Ici les futures routes */
+
+app.get('*', (req, res) => {
+    return res.status(404).json({ message: 'Page not found' })
+})
+
+
+
+
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json()); //Permet de lire du json. Ne pas oublier d'avoir le header Content-Type avec comme valeur application/json
+//https://developer.mozilla.org/fr/docs/Web/HTTP/Headers/Content-Type
+
+//Routes
+app.get('/', (req, res) => {
+    res.send([
+        "<h1>E'Chat</h1>"
+    ].join(''));
+});
+
+/* Formulaire de connexion */
+app.post('/login', (req, res) => {
+    // Pas d'information à traiter
+    if (!req.body.username || !req.body.password) {
+        return res.status(400).json({ message: 'Error. Please enter the correct username and password' })
+    }
+
+    // Checking
+    const user = users.find(u => u.username === req.body.username && u.password === req.body.password)
+
+    // Pas bon
+    if (!user) {
+        return res.status(400).json({ message: 'Error. Wrong login or password' })
+    }
+
+    const token = jwt.sign({
+        id: user.id,
+        username: user.username
+    }, SECRET, { expiresIn: '3 hours' })
+
+    return res.json({ access_token: token })
+})
+
+
+const channelRoutes = require('./app/routes/channel_routes');
+const userRoutes = require('./app/routes/user_routes');
+const messageRoutes = require('./app/routes/message_routes');
+
+app.use('/api/v1/channels', channelRoutes);
+app.use('/api/v1/users', userRoutes);
+app.use('/api/v1', messageRoutes);
+
+module.exports = app;
